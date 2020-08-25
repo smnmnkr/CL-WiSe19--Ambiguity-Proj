dfc9a0124853ea14bc3c6dc00848ad515236ec9a
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-08-25 16:34:06 +0200

    added method to calculate ambiguity over a range

---
+++ automata/tasks.py
@@ -0,0 +1,34 @@
+from automata.nfa import NFA
+from automata.util import create_permutations
+
+
+# -------- ambiguity_over_range -----------
+#
+def ambiguity_over_range(
+        automaton: NFA,
+        maximum: int,
+        minimum: int = 1,
+) -> None:
+
+    for n in range(minimum, maximum + 1):
+        word, max_path_num = degree_of_ambiguity(automaton, n)
+
+        print(f"length: {n:02}\t ambiguity: {max_path_num}")
+
+
+# -------- degree_of_ambiguity -----------
+#
+def degree_of_ambiguity(automaton: NFA, length: int) -> tuple:
+
+    max_word: str = ''
+    max_path_num: int = 0
+
+    for word in create_permutations(automaton.alphabet, length):
+
+        word_ambiguity: int = automaton.ambiguity(word)
+
+        if (max_path_num < word_ambiguity):
+            max_word = word
+            max_path_num = word_ambiguity
+
+    return word, max_path_num
