0f79732a1c7aa461c6c728d7e4a04e8f5ea4023f
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-09-01 19:20:45 +0200

    bumped version to 0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='automata',
-    version='0.2.3',
+    version='0.2.4',
     packages=find_packages(),
     entry_points={"console_scripts": ["automata = main:main"]},
 )
