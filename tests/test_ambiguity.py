8919d7955e4004ab686c2f3553b7f7baf5a21f13
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-09-01 19:01:14 +0200

    added ambiguity test suite

---
+++ tests/test_ambiguity.py
@@ -0,0 +1,15 @@
+import pytest
+
+from automata.nfa import NFA
+from automata.ambiguity import ambiguity_over_range, degree_of_ambiguity
+
+automaton: NFA = NFA(**pytest.automata_config)
+
+
+def test_degree_of_ambiguity():
+    assert degree_of_ambiguity(automaton, 1) == ('a', 2)
+
+
+def test_ambiguity_over_range():
+    assert ambiguity_over_range(automaton, 3) == [('a', 2), ('aa', 1),
+                                                  ('aaa', 1)]
