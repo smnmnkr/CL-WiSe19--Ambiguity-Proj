17cf347c381e98e418900133d3a387898742fa7d
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-09-01 19:01:37 +0200

    moved automata config to global test config

--- tests/test_nfa.py
+++ tests/test_nfa.py
@@ -1,21 +1,8 @@
+import pytest
+
 from automata.nfa import NFA
 
-config: dict = {
-    'states': ['q0', 'q1', 'q2', 'q3'],
-    'alphabet': ['a'],
-    'transitions': {
-        'q0': {
-            'a': ['q1', 'q2', 'q3']
-        },
-        'q1': {
-            'a': ['q1']
-        }
-    },
-    'initial_state': 'q0',
-    'final_states': ['q1', 'q2'],
-}
-
-automaton: NFA = NFA(**config)
+automaton: NFA = NFA(**pytest.automata_config)
 
 word: str = 'a'
 
@@ -29,7 +16,7 @@ def test_init():
 
 
 def test_export():
-    assert automaton.export() == config
+    assert automaton.export() == pytest.automata_config
 
 
 def test_process():
