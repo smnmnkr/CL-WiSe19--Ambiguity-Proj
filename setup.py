c99628be44a47979be5914dc9976a713f9a44612
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-09-01 19:03:54 +0200

    linting

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name='automata', version='0.2.3', packages=find_packages())
+setup(
+    name='automata',
+    version='0.2.3',
+    packages=find_packages(),
+)
