245801aaa06fb93f4000206612e803f56541af1c
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-09-03 16:12:07 +0200

    added time tracking to task

--- automata/util.py
+++ automata/util.py
@@ -1,9 +1,29 @@
 import itertools
 
+from functools import wraps
+from time import time
 
 # -------- create_permutations -----------
 #
+
+
 def create_permutations(chars: list, n: int):
 
     for item in itertools.product(chars, repeat=n):
         yield "".join(item)
+
+
+# -------- time_track -----------
+#
+def time_track(func):
+    @wraps(func)
+    def wrap(*args, **kw):
+
+        t_start = time()
+        result = func(*args, **kw)
+        t_end = time()
+
+        duration = t_end - t_start
+
+        return result, duration
+    return wrap
