28d4b142c2d64e578ee3fce7fba887523f865c63
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-09-03 16:50:34 +0200

    release 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='automata',
-    version='0.2.4',
+    version='0.3.0',
     packages=find_packages(),
     entry_points={"console_scripts": ["automata = main:main"]},
 )
