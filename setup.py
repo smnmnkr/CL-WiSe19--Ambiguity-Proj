58e278eb1b8970fc98237c149cbe93782c77b30a
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-09-07 16:57:21 +0200

    bumped minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="automata",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(),
     entry_points={"console_scripts": ["automata = main:main"]},
 )
