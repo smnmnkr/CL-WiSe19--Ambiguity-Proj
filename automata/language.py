1cc91b4c9d052df99abbe90d90ced2fb084f6b93
Simon Münker <56649035+smnmnkr@users.noreply.github.com>
2020-09-22 09:36:41 +0200

    init language functions

---
+++ automata/language.py
@@ -0,0 +1,22 @@
+from automata.nfa import NFA
+from automata.util import create_permutations
+
+
+#  -------- language_over_range -----------
+#
+def language_over_range(automaton: NFA, maximum: int, minimum: int = 1) -> list:
+
+    return [
+        generate_language(automaton, n) for n in range(minimum, maximum + 1)
+    ]
+
+
+#  -------- generate_language -----------
+#
+def generate_language(automaton: NFA, length: int) -> list:
+
+    return [
+        x
+        for x in create_permutations(automaton.alphabet, length)
+        if automaton.accepts(x) != (False, [])
+    ]
